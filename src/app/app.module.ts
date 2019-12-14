9098bd572c15edd213690efc2d6ccd79c4de2852
l.hussain@matellio.com <l.hussain@matellio.com>
2019-12-14 11:56:01 +0530

    Changes by liyakat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -18,7 +18,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
+  username:string;
+  password:string;
+  constructor(private router:Router) { }
 
   ngOnInit() {
+
+  }
+
+  login(){
+    console.log("sdsd")
+    if(this.username === '' && this.password === ''){
+      alert("Please enter Username and Password");
+      return;
+    }else if(this.username === 'admin' && this.password === 'admin'){
+      this.router.navigate(['/dashboard']);
+    }else{
+      alert("Invalid Username or Password");
+      return;
+    }
   }
 
 }
