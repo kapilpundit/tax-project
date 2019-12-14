f2cfb2d8c0cb1c6ceb6d89382d580124357aeb1b
Kapil Sharma <kapil.pundit@gmail.com>
2019-12-15 03:14:20 +0530

    CHanged login screen

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
 import { ReCaptchaV3Service } from 'ng-recaptcha';
+
+declare var $:any;
+declare var jQuery:any;
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent implements OnInit, AfterViewInit {
   username:string;
   password:string;
   
@@ -16,6 +19,97 @@ export class SignInComponent implements OnInit {
     private recaptchaV3Service: ReCaptchaV3Service) { }
 
   ngOnInit() {
+    
+  }
+
+  ngAfterViewInit() {
+    (function ($) {
+      "use strict";
+
+      /*==================================================================
+      [ Focus input ]*/
+      $('.input100').each(function(){
+          $(this).on('blur', function(){
+              if($(this).val().trim() != "") {
+                  $(this).addClass('has-val');
+              }
+              else {
+                  $(this).removeClass('has-val');
+              }
+          })    
+      })
+
+
+      /*==================================================================
+      [ Validate ]*/
+      var input = $('.validate-input .input100');
+
+      $('.validate-form').on('submit',function(){
+          var check = true;
+
+          for(var i=0; i<input.length; i++) {
+              if(validate(input[i]) == false){
+                  showValidate(input[i]);
+                  check=false;
+              }
+          }
+
+          return check;
+      });
+
+
+      $('.validate-form .input100').each(function(){
+          $(this).focus(function(){
+            hideValidate(this);
+          });
+      });
+
+      function validate (input) {
+          if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+              if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                  return false;
+              }
+          }
+          else {
+              if($(input).val().trim() == ''){
+                  return false;
+              }
+          }
+      }
+
+      function showValidate(input) {
+          var thisAlert = $(input).parent();
+
+          $(thisAlert).addClass('alert-validate');
+      }
+
+      function hideValidate(input) {
+          var thisAlert = $(input).parent();
+
+          $(thisAlert).removeClass('alert-validate');
+      }
+      
+      /*==================================================================
+      [ Show pass ]*/
+      var showPass = 0;
+      $('.btn-show-pass').on('click', function(){
+          if(showPass == 0) {
+              $(this).next('input').attr('type','text');
+              $(this).find('i').removeClass('zmdi-eye');
+              $(this).find('i').addClass('zmdi-eye-off');
+              showPass = 1;
+          }
+          else {
+              $(this).next('input').attr('type','password');
+              $(this).find('i').addClass('zmdi-eye');
+              $(this).find('i').removeClass('zmdi-eye-off');
+              showPass = 0;
+          }
+          
+      });
+
+
+    })(jQuery);
   }
 
   recaptchaCode(): void {
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ReCaptchaV3Service } from 'ng-recaptcha';
+import { Router } from '@angular/router';
+
+declare var $:any;
+declare var jQuery:any
 
 
 @Component({
@@ -7,13 +11,104 @@ import { ReCaptchaV3Service } from 'ng-recaptcha';
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, AfterViewInit {
 
   constructor(
+    private router:Router,
     private recaptchaV3Service: ReCaptchaV3Service
   ) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    (function ($) {
+      "use strict";
+
+      /*==================================================================
+      [ Focus input ]*/
+      $('.input100').each(function(){
+          $(this).on('blur', function(){
+              if($(this).val().trim() != "") {
+                  $(this).addClass('has-val');
+              }
+              else {
+                  $(this).removeClass('has-val');
+              }
+          })    
+      })
+
+
+      /*==================================================================
+      [ Validate ]*/
+      var input = $('.validate-input .input100');
+
+      $('.validate-form').on('submit',function(){
+          var check = true;
+
+          for(var i=0; i<input.length; i++) {
+              if(validate(input[i]) == false){
+                  showValidate(input[i]);
+                  check=false;
+              }
+          }
+
+          return check;
+      });
+
+
+      $('.validate-form .input100').each(function(){
+          $(this).focus(function(){
+            hideValidate(this);
+          });
+      });
+
+      function validate (input) {
+          if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+              if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                  return false;
+              }
+          }
+          else {
+              if($(input).val().trim() == ''){
+                  return false;
+              }
+          }
+      }
+
+      function showValidate(input) {
+          var thisAlert = $(input).parent();
+
+          $(thisAlert).addClass('alert-validate');
+      }
+
+      function hideValidate(input) {
+          var thisAlert = $(input).parent();
+
+          $(thisAlert).removeClass('alert-validate');
+      }
+      
+      /*==================================================================
+      [ Show pass ]*/
+      var showPass = 0;
+      $('.btn-show-pass').on('click', function(){
+          if(showPass == 0) {
+              $(this).next('input').attr('type','text');
+              $(this).find('i').removeClass('zmdi-eye');
+              $(this).find('i').addClass('zmdi-eye-off');
+              showPass = 1;
+          }
+          else {
+              $(this).next('input').attr('type','password');
+              $(this).find('i').addClass('zmdi-eye');
+              $(this).find('i').removeClass('zmdi-eye-off');
+              showPass = 0;
+          }
+          
+      });
+
+
+    })(jQuery);
   }
 
   recaptchaCode(): void {
@@ -21,7 +116,7 @@ export class SignUpComponent implements OnInit {
       .subscribe((token) => console.log(token));
   }
 
-  register() {
+  signup() {
     this.recaptchaCode();
   }
 
