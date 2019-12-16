0392218c58f24bf1f0e5e28fec13bcee8a36b068
Liyakat Hussain <l.hussain@matellio.com>
2019-12-16 10:13:20 +0530

    changes for home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
+declare var jQuery:any;
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,17 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(window).scroll(function() {    
+      var scroll = $(window).scrollTop();
+  
+       //>=, not <=
+      if (scroll >= 500) {
+          //clearHeader, not clearheader - caps H
+          $(".top-bar").removeClass("top-bar--top");
+      }else{
+        $(".top-bar").addClass("top-bar--top");
+      }
+  }); //missing );
   }
 
 }
